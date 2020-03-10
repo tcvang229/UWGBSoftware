5b28042aa79031d3d07557a62a4db0711989954f
Dylan Hoffman <jkioicefire@gmail.com>
2020-03-09 21:39:33 -0500

    Revert "removed ChuFu's lab1 that he committed"
    This reverts commit 367b6acbf29f2f0be280997fb87fdd78093e683d.
    
    Reverting to before gitignore changes

---
+++ lab1/BluetoothTraditional.java
@@ -0,0 +1,12 @@
+package lab1;
+
+public class BluetoothTraditional extends Bluetooth{
+    public BluetoothTraditional() {
+        super.setType("Bluetooth Traditional");
+    }
+
+    @Override
+    public void connect() {
+        // do some connecting with bluetooth API
+    }
+}
